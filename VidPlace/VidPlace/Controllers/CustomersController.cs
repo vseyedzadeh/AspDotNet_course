b5039410fbf3dd753d165e47016117ecb35b3db9
DESKTOP-RE03UC2\vseye <v.seyedzadeh.985@gmail.com>
2017-12-06 00:17:01 -0500

    **Modify VidPlace Project

--- VidPlace/VidPlace/Controllers/CustomersController.cs
+++ VidPlace/VidPlace/Controllers/CustomersController.cs
@@ -10,15 +10,31 @@ namespace VidPlace.Controllers
     public class CustomersController : Controller
     {
         // GET: Customer
-        public ActionResult getCustomer()
+        public ActionResult Index()
         {
-            var customer = new Customer()
-            {
-                Name = "Azi",
-                Address = "Montreal, QC, Canada"
-            };
+            IEnumerable<Customer> customer = getCustomers();
 
             return View(customer);
         }
+
+        public ActionResult Detail(int customerID)
+        {
+            var customers = getCustomers().FirstOrDefault(x => x.ID == customerID);
+            if(customers == null)
+                throw new HttpException(404, "Some description");
+            return View(customers);
+        }
+
+
+        private static IEnumerable<Customer> getCustomers()
+        {
+            return new List<Customer>
+            {
+                new Customer(){ ID = 1, Name = "vaji", Address = "Montreal, QC, Canada" },
+                new Customer(){ ID = 2, Name = "Azi", Address = "Montreal, QC, Canada"},
+                new Customer(){ ID = 3, Name = "Mehrshad", Address = "Montreal"}
+            };
+        }
+
     }
 }
